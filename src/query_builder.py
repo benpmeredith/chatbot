929f9f750196c9da3b8fc3e96d403302a0bafee6
Mohammed Ansari <ansari@spokesly.com>
2019-05-11 15:35:20 -0700

    Query builder: Adding option for speech input in main

--- src/query_builder.py
+++ src/query_builder.py
@@ -18,13 +18,14 @@ class QueryBuilder:
     male_subjects = ["son", "nephew", "grandson"]
     female_subjects = ["daughter", "niece", "granddaughter"]
 
-    def __init__(self):
+    def __init__(self, recognizer, source):
         self.lemmatizer = Lemmatizer(LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES)
         self.engine = pyttsx3.init()
         self.engine.setProperty('voice', 'com.apple.speech.synthesis.voice.ava.premium')
         self.engine.setProperty('rate', self.engine.getProperty('rate') - 31.5)
         self.nlp = spacy.load("en_core_web_sm")
-        self.recognizer = sr.Recognizer()
+        self.recognizer = recognizer
+        self.source = source
 
     def form_query(self, sentence=None):
         known_subjects = ["child", "son", "daughter", "nephew", "grandson", "granddaughter", "niece"]
@@ -34,55 +35,58 @@ class QueryBuilder:
 
         lemmatizer = Lemmatizer(LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES)
 
-        with sr.Microphone() as source:
-            self.recognizer.adjust_for_ambient_noise(source)
-            doc = self.nlp(sentence)
+        doc = self.nlp(sentence)
 
-            noun_subjects = []
-            for chunk in doc.noun_chunks:
-                poss = False
-                noun_subject = lemmatizer(chunk.root.text, u"NOUN")[0]
-                for child in chunk.root.children:
-                    if "poss" in child.dep_:
-                        poss = child.text
-                noun_subjects.append((noun_subject, poss))
-            print(noun_subjects)
+        noun_subjects = []
+        for chunk in doc.noun_chunks:
+            poss = False
+            noun_subject = lemmatizer(chunk.root.text, u"NOUN")[0]
+            for child in chunk.root.children:
+                if "poss" in child.dep_:
+                    poss = child.text
+            noun_subjects.append((noun_subject, poss))
+        print(noun_subjects)
 
-            root_verbs = [lemmatizer(token.head.text, u"VERB")[0] for token in doc if
-                          token.dep_ == "ROOT" and token.head.pos_ == "VERB"]
-            print(root_verbs)
+        root_verbs = [lemmatizer(token.head.text, u"VERB")[0] for token in doc if
+                      token.dep_ == "ROOT" and token.head.pos_ == "VERB"]
+        print(root_verbs)
 
-            q = Query(self.engine, self.recognizer, source)
+        q = Query(self.engine, self.recognizer, self.source)
 
-            for ns, poss in noun_subjects:
-                if ns in known_subjects:
-                    q.subject = ns
-                    q.subject_poss = poss
-                    break
-                else:
-                    print(f"Sorry, we don't support questions on {ns} yet")
-                    return
+        for ns, poss in noun_subjects:
+            if ns in known_subjects:
+                q.subject = ns
+                q.subject_poss = poss
+                break
+            else:
+                print(f"Sorry, we don't support questions on {ns} yet")
+                return
 
-            for ns, _ in noun_subjects:
-                if ns in male_subjects:
-                    q.subject_gender = "male"
-                elif ns in female_subjects:
-                    q.subject_gender = "female"
-                    break
+        for ns, _ in noun_subjects:
+            if ns in male_subjects:
+                q.subject_gender = "male"
+            elif ns in female_subjects:
+                q.subject_gender = "female"
+                break
 
-            for verb in root_verbs:
-                if verb in known_verbs:
-                    q.verb = verb
-                    break
+        for verb in root_verbs:
+            if verb in known_verbs:
+                q.verb = verb
+                break
 
-            print(q)
-            q.get_missing_fields()
-            print(q)
+        print(q)
+        q.get_missing_fields()
+        print(q)
 
 
 def main():
-    qb = QueryBuilder()
-    qb.form_query("My nephew is being bullied")
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        qb = QueryBuilder(recognizer, source)
+        initial_sentence = speech2text(recognizer, source)
+        # initial_sentence = "My nephew is being bullied"
+        qb.form_query(initial_sentence)
 
 
 if __name__ == "__main__":
